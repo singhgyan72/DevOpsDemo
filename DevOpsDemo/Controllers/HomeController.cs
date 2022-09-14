81d94c1c236240d5ec99b160e68f72dde89b9da2
singhgyan72 <singhgyan72@gmail.com>
2022-09-14 16:08:11 +0530

    first commit

---
+++ DevOpsDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DevOpsDemo.Models;
+using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+        private readonly IPostRepository _postRepository;
+
+        public HomeController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _postRepository.GetPosts();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DevOpsDemo/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevOpsDemo.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureAppServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPostRepository, PostRepository>();
+        }
+    }
+}
